4f10de0837fcc811a323b49d6ff7c384238f7126
Sergio Mansano <mansano71@gmail.com>
2018-07-16 16:02:39 -0300

    Projeto criado

---
+++ src/main/java/com/sergiomansano/checkcar/CheckCarApplication.java
@@ -0,0 +1,12 @@
+package com.sergiomansano.checkcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckCarApplication.class, args);
+	}
+}
